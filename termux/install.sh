86041a6b0c86a05239f8c472fcc9301774333fe5
AkiJoey <akijoey1010635951@gmail.com>
2020-05-15 20:16:32 +0800

    default commit

---
+++ termux/install.sh
@@ -0,0 +1,29 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt update
+apt install -y vim curl wget openssh git zsh
+
+git clone git://github.com/ohmyzsh/oh-my-zsh $HOME/.oh-my-zsh --depth 1
+cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/' $HOME/.zshrc
+
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions --depth 1
+git clone git://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting --depth 1
+sed -i 's/plugins=(git/& zsh-autosuggestions zsh-syntax-highlighting/' $HOME/.zshrc
+
+chsh -s zsh
+
+# wget -P $HOME/.termux 
+# wget -P $HOME/.termux 
+
+# wget -P $PREFIX/etc/motd -N
+
+apt install -y screenfetch
+echo 'screenfetch' >> $HOME/.zshrc
+
+termux-setup-storage
+
+echo 'Install successed.'
+echo 'Please restart Termux.'
+
+exit
