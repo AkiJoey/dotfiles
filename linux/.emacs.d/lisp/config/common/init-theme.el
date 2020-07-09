d8d6d4839eb1e9ca8bf0722af893c59f70d53bc1
AkiJoey <akijoey1010635951@gmail.com>
2020-07-10 02:54:20 +0800

    feat: emacs config

---
+++ linux/.emacs.d/lisp/config/common/init-theme.el
@@ -0,0 +1,8 @@
+;; theme load path
+(add-to-list 'custom-theme-load-path "~/.emacs.d/list/theme")
+
+;; load theme
+(load-theme 'atom-one-dark t)
+
+;; provide
+(provide 'init-theme)
---
+++ linux/.emacs.d/lisp/config/init-config.el
@@ -0,0 +1,8 @@
+;; init common
+(require 'init-ui)
+(require 'init-theme)
+
+;; init plugin
+
+;; provide
+(provide 'init-config)
